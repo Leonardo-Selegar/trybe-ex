a2744a6b57995a7955ca27d935808b86f7368f68
Leonardo Selegar <leonardo11132@gmail.com>
2022-06-22 18:36:18 -0300

    Ex5 Foguetinho

---
+++ 1-Fundamentos/JavaScript/4.2/ex5/scripts.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+numeroMax = 0;
+
+for(let cont = 0; cont <numbers.length; cont++) {
+  numeroMax = Math.max(numeroMax,numbers[cont]);
+}
+console.log(numeroMax);
+
+
