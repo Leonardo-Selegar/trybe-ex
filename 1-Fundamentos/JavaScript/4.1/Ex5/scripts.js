62e318274885e5b7c24d586f493b6d4e08085a52
Leonardo Selegar <leonardo11132@gmail.com>
2022-06-26 03:18:31 -0300

    Ex6

--- 1-Fundamentos/JavaScript/4.1/Ex5/scripts.js
+++ 1-Fundamentos/JavaScript/4.1/Ex5/scripts.js
@@ -1,9 +1,15 @@
 const angulo1 = 60;
 const angulo2 = 60;
-const angulo3 = 620;
+const angulo3 = -60;
+let angulos = angulo1 + angulo2 + angulo3;
 
-if(angulo1 + angulo2 + angulo3 === 180){
-    console.log('true');
+let angulosPositivos = angulo1 > 0 && angulo2 > 0 && angulo3 > 0;
+if(angulosPositivos){
+    if(angulos === 180){
+        console.log('true');
+    }else{
+        console.log('false');
+    }
 }else{
-    console.log('Erro: Não é um triangulo!');
+    console.log("Erro: Angulo negativo!");
 }
---
+++ 1-Fundamentos/JavaScript/4.1/Ex6/scripts.js
@@ -0,0 +1,24 @@
+let peça = 'RaiNha';
+
+switch(peça.toLowerCase()){
+    case 'rei':
+        console.log('Rei-> Uma casa apenas para qualquer direção.');
+         break;
+    case 'rainha':
+        console.log('Rainha-> Diagonal, horizontal e vertical.');
+        break;
+    case 'bispo':
+        console.log('Bispo-> Diagonal.');
+        break;
+    case 'cavalo':
+        console.log('Cavalo-> "L" pode pular sobre as peças.');
+    case 'torre':
+        console.log('Torre -> Horizontal e vertical.')
+        break;
+    case 'peão':
+        console.log('Apenas uma casa para frente, no primeiro movimento podem ser duas casas.');
+        break;
+    default:
+        console.log('Peça invalida!');
+        break;
+}
