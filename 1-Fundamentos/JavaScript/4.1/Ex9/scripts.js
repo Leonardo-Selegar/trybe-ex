953af99047592cfc25c4a82e38ae93d6aae97a4b
Leonardo Selegar <leonardo11132@gmail.com>
2022-06-26 03:33:11 -0300

    Ex9

---
+++ 1-Fundamentos/JavaScript/4.1/Ex9/scripts.js
@@ -0,0 +1,11 @@
+const n1 = 12;
+const n2 = 6;
+const n3 = 1;
+resposta = false;
+
+if(n1 % 2 === 1 || n2 % 2 === 1 || n3 % 2 === 1){
+    resposta = true;
+    console.log(resposta);
+}else{
+    console.log(resposta);
+}
