26e3f64903bd2a0e51e7beee8b48b2e9469ac5c2
Leonardo Selegar <leonardo11132@gmail.com>
2022-06-26 02:22:46 -0300

    Ex2

---
+++ 1-Fundamentos/JavaScript/4.1/Ex2/scripts.js
@@ -0,0 +1,10 @@
+const n1 = 24;
+const n2 = 224;
+
+if(n1 > n2){
+    console.log('O primeiro numero é maior!');
+}else if(n1 < n2){
+    console.log('O segundo numero é maior!');
+}else{
+    console.log('Os numeros são iguais!');
+}
