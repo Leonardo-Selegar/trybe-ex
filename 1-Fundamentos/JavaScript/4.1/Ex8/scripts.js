d1036e8a5125487ef61f870c47dee1cc34c52471
Leonardo Selegar <leonardo11132@gmail.com>
2022-06-26 03:31:06 -0300

    Ex8

---
+++ 1-Fundamentos/JavaScript/4.1/Ex8/scripts.js
@@ -0,0 +1,11 @@
+const n1 = 12;
+const n2 = 5;
+const n3 = 1;
+resposta = false;
+
+if(n1 % 2 === 0 || n2 % 2 === 0 || n3 % 2 === 0){
+    resposta = true;
+    console.log(resposta);
+}else{
+    console.log(resposta);
+}
