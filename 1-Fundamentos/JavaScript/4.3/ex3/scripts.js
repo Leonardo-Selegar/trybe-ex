708c559d2c149530aeac6ca30d3732014f1ab37e
Leonardo Selegar <leonardo11132@gmail.com>
2022-06-26 23:50:02 -0300

    Ex4

--- 1-Fundamentos/JavaScript/4.3/ex3/scripts.js
+++ 1-Fundamentos/JavaScript/4.3/ex3/scripts.js
@@ -1,13 +1,13 @@
 let array = ["java", "javascript", "python", "html", "css"];
-// let maiorPalavra = array[0];
+let maiorPalavra = array[0];
 
-// for (let index = 0; index < array.length; index++) {
-//   if (array[index].length > maiorPalavra.length) {
-//     maiorPalavra = array[index];
-//   }
-// }
+for (let index = 0; index < array.length; index++) {
+  if (array[index].length > maiorPalavra.length) {
+    maiorPalavra = array[index];
+  }
+}
 
-// console.log(maiorPalavra);
+console.log(maiorPalavra);
 
 let menorPalavra = array[0];
 
