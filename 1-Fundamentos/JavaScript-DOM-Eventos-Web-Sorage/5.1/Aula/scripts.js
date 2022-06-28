452a9cdcdbd5f1645eb31cee5cd3e37fcd62cc75
Leonardo Selegar <leonardo11132@gmail.com>
2022-06-28 15:27:53 -0300

    5.1 aula ex1

---
+++ 1-Fundamentos/JavaScript-DOM-Eventos-Web-Sorage/5.1/Aula/scripts.js
@@ -0,0 +1,9 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+const secondParagraph = document.getElementById("second-paragraph");
+secondParagraph.style.color = "blue";
+
+document.getElementById('page-title').innerText = 'De volta para o futuro!';
+document.getElementById("second-paragraph").innerText = 'Melhor filme de viagem no tempo de todos os tempos kk';
+document.getElementById("subtitle").innerText = 'Filmes de viagem no tempo!';
