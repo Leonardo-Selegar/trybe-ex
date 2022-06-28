a26def7810588f65af035fc6a143ab8e72c47c98
Leonardo Selegar <leonardo11132@gmail.com>
2022-06-28 16:05:59 -0300

    Ex2 aula 5.1

---
+++ 1-Fundamentos/JavaScript-DOM-Eventos-Web-Sorage/5.1/Aula/ex2/scripts.js
@@ -0,0 +1,9 @@
+const paragraph = document.getElementsByClassName('paragraph')[0];
+paragraph.style.color = "red";
+
+const subtitle = document.getElementsByTagName('h4')[0];
+subtitle.style.color = "blue";
+
+console.log(document.getElementsByClassName('paragraph'))
+
+console.log(document.getElementsByTagName('subtitle'))
