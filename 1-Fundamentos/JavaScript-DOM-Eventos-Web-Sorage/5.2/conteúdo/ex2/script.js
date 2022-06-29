0acaeb5015d97a28554d2fe008c824fca3a440b8
Leonardo Selegar <leonardo11132@gmail.com>
2022-06-29 15:58:52 -0300

    Ex2 conteudo

---
+++ 1-Fundamentos/JavaScript-DOM-Eventos-Web-Sorage/5.2/conteúdo/ex2/script.js
@@ -0,0 +1,11 @@
+const pai = document.getElementById('pai');
+const irmãoElemententoOndeVoceEsta = document.createElement('section');
+
+irmãoElemententoOndeVoceEsta.id = 'irmãoElemententoOndeVoceEsta';
+pai.appendChild(irmãoElemententoOndeVoceEsta);
+
+const ElemententoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta')
+const filhoElementoOndeVoceEsta = document.createElement('section');
+
+filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
+ElemententoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta)
