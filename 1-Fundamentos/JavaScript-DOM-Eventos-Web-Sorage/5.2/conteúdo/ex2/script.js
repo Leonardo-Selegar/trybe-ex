dc85a28f4046768067c83b4156ea7e736e70eb1a
Leonardo Selegar <leonardo11132@gmail.com>
2022-06-30 13:08:42 -0300

    Ex 5.2

--- 1-Fundamentos/JavaScript-DOM-Eventos-Web-Sorage/5.2/conteúdo/ex2/script.js
+++ 1-Fundamentos/JavaScript-DOM-Eventos-Web-Sorage/5.2/conteúdo/ex2/script.js
@@ -1,11 +1,17 @@
 const pai = document.getElementById('pai');
 const irmãoElemententoOndeVoceEsta = document.createElement('section');
-
 irmãoElemententoOndeVoceEsta.id = 'irmãoElemententoOndeVoceEsta';
 pai.appendChild(irmãoElemententoOndeVoceEsta);
 
-const ElemententoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta')
+const elemententoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta')
 const filhoElementoOndeVoceEsta = document.createElement('section');
-
 filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
-ElemententoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta)
+elemententoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+
+const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+const filhoDoPrimeiroFilhoDoFilho = document.createElement('section');
+filhoDoPrimeiroFilhoDoFilho.id ='filhoDoPrimeiroFilhoDoFilho';
+primeiroFilhoDoFilho.appendChild(filhoDoPrimeiroFilhoDoFilho);
+
+const terceiroFIlho = document.getElementById('filhoDoPrimeiroFilhoDoFilho').parentElement.parentElement.nextElementSibling;
+
